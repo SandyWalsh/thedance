d41b252005cae9b1647380950325be23d777f750
Sandy Walsh <sandy.walsh@rackspace.com>
2014-05-29 00:04:38 -0300

    Grid structures

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ thedance
 ========
 
 Sequence scripting experiment
+
+Each cell in the grid is a Participant. The output shows the target direction
+at that moment in time. 
+
+    $ python dance.py 
+              0   1   2   3   4
+         0.  11 191 233 121 153
+         1. 170 160  84  63 129
+         2. 329 116 261  86 166
+         3. 315 168 359  49 321
+         4. 241 254 316 314 202
+
---
+++ dance.py
@@ -0,0 +1,84 @@
+import random
+
+
+class Participant(object):
+    def __init__(self):
+        self.sequence = []
+        self.current_direction = random.randrange(360)
+
+    def add(self, dot):
+        self.sequence.append(dot)
+
+    def __str__(self):
+        return str(self.current_direction)
+
+
+class Grid(object):
+    def __init__(self, size_x, size_y):
+        self.size_x = size_x
+        self.size_y = size_y
+        self.grid = [[Participant() for x in range(size_x)] for x in range(size_y)]
+
+    def set(self, x, y, dot):
+        self.grid[y][x] = dot
+
+    def get(self, x, y):
+        return self.grid[y][x]
+
+    def move_dots(self):
+        pass
+
+    def dump(self):
+        for x, row in enumerate(self.grid):
+            if x == 0:
+                print "  ",
+                for y, col in enumerate(row):
+                    print "%3d" % y,
+                print
+            for y, col in enumerate(row):
+                if y == 0:
+                    print "%d." % x,
+                print "%3s" % str(col),
+            print
+
+
+grid = Grid(5, 5)
+grid.dump()
+
+
+class Movement(object):
+    def __init__(self):
+        self.target_direction = direction  # 0-359
+        self.movement = 0  # +1 (clockwise) or -1 (counter clockwise)
+        self.speed = 0  # degrees per tick
+
+        self.ticks = 250  # ms
+
+    def _distance_to_target(self, moving):
+        """Compute the distance to the target if
+           we're moving in a clockwise or counterclockwise
+           direction."""
+        current_direction = self.participant.current_direction
+        if moving > 0:
+            if current_direction < self.target_direction:
+                return 360 - current_direction + self.target_direction
+            return self.target_direction - current_direction
+        else:
+            if current_direction < self.target_direction:
+                return current_direction - self.target_direction
+            return 360 - self.target_direction + current_direction
+
+
+    def move_to_direction(self, target_direction, movement, millis):
+        # millis = 250 would mean instant transportation to target.
+        if millis < self.ticks:
+            raise Exception("Smallest time is %dms" % self.ticks)
+        self.target_direction = target_direction
+        self.movement = movement
+        self.speed = self._distance_to_target(movement) / (millis / self.ticks)
+
+    def start(self, participant):
+        self.participant = participant
+
+    def increment(self):
+        self.participant.current_position + (self.speed * self.movement)
